9fc86fbd11a30c68263dc34272b0e6a60aa23bd3
wy <thelmg@163.com>
2023-01-08 20:49:55 +0800

    create workspace

---
+++ packages/connector/rollup.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "rollup";
+
+export default defineConfig({
+  input: `src/index.ts`,
+  output: [{ file: "dist", format: "esm" }],
+  plugins: [],
+  watch: {
+    include: "src/**",
+  },
+  external: ["ethers"],
+});
